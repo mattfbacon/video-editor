006aeaa6bb4deac7a5d9fa1cb932441b1fb7991d
Matt Fellenz <mattf53190@gmail.com>
2021-05-10 18:51:49 -0700

    Basic GTKmm Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <gtkmm.h>
 
-int main () {
-	std::cout << "React bad" << std::endl;
-	return 0;
+class MyWindow : public Gtk::Window {
+public:
+	MyWindow(){
+		auto test_label = new Gtk::Label("React Bad");
+		set_title("Eternal Truth");
+		set_default_size(200, 200);
+		add(*test_label);
+		show_all();
+	}
+};
+
+int main(int argc, char* argv[]) {
+	auto application = Gtk::Application::create("com.branhamcodes.videoeditor");
+	MyWindow window;
+	application->run(window, argc, argv);
+
+	return EXIT_SUCCESS;
 }
